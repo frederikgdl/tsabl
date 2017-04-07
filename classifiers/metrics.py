99163a19e503c0867fdbebb7f031ed015379af5d
frederikgdl <frederikgdl@gmail.com>
2017-04-07 16:29:28 +0200

    Add metrics for neutrals, macro f1 and clean code

--- classifiers/metrics.py
+++ classifiers/metrics.py
@@ -1,63 +1,57 @@
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 
-import classifiers.funcs as funcs
+POSITIVE = 1
+NEUTRAL = 0
+NEGATIVE = -1
 
 
-# Returns precision given predictions and labels
-# Change current class with parameter version
-# Converts neutrals to opposite of docs wanted (to -1 if current class is positives)
-# predictions_num is vector with 1 for positive samples, 0 for neutral and -1 for negative
-# labels_num is vector with 1 for positive samples, 0 for neutral and -1 for negative
-def calc_precision_with_neutrals(predictions_num, labels_num, version="pos"):
-    if version == "neg":
-        rel_docs = -1
-    else:
-        rel_docs = 1
-
-    # Change neutrals to opposite of docs wanted
-    processed_true_vals = rel_docs * funcs.remove_neutrals(labels_num, rel_docs)
-    processed_pred_vals = rel_docs * funcs.remove_neutrals(predictions_num, rel_docs)
-
-    return precision_score(y_true=processed_true_vals, y_pred=processed_pred_vals)
+# Calculates the F1_PN-score for the given predictions and labels
+# F1_PN is the average of F1-scores for positives and negatives
+def f1_pn_score(predictions, labels):
+    f1_pos = ternary_f1_score(predictions, labels, POSITIVE)
+    f1_neg = ternary_f1_score(predictions, labels, NEGATIVE)
+    return (f1_pos + f1_neg) / 2
 
 
-# Returns recall given predictions and labels
-# Change current class with parameter version
-# Converts neutrals to opposite of docs wanted (to -1 if current class is positives)
-# predictions_num is vector with 1 for positive samples, 0 for neutral and -1 for negative
-# labels_num is vector with 1 for positive samples, 0 for neutral and -1 for negative
-def calc_recall_with_neutrals(predictions_num, labels_num, version="pos"):
-    if version == "neg":
-        rel_docs = -1
-    else:
-        rel_docs = 1
-
-    # Change neutrals to opposite of docs wanted
-    processed_true_vals = rel_docs * funcs.remove_neutrals(labels_num, rel_docs)
-    processed_pred_vals = rel_docs * funcs.remove_neutrals(predictions_num, rel_docs)
-
-    return recall_score(y_true=processed_true_vals, y_pred=processed_pred_vals)
+def ternary_macro_f1_score(predictions, labels):
+    f1_pos = ternary_f1_score(predictions, labels, POSITIVE)
+    f1_neg = ternary_f1_score(predictions, labels, NEGATIVE)
+    f1_neu = ternary_f1_score(predictions, labels, NEUTRAL)
+    return (f1_pos + f1_neg + f1_neu) / 3
 
 
-# Returns F1-score given predictions and labels_num
-# Special version of F1-score, average of F-scores for positives and negatives
-# predictions_num is vector with 1 for positive samples, 0 for neutral and -1 for negative
-# labels_num is vector with 1 for positive samples, 0 for neutral and -1 for negative
-def calc_F1_score_with_neutrals(predictions_num, labels_num):
-    precision_pos = calc_precision_with_neutrals(predictions_num, labels_num, version="pos")
-    recall_pos = calc_recall_with_neutrals(predictions_num, labels_num, version="pos")
-    F1_pos = calc_F1_score(precision_pos, recall_pos)
+# Calculates F1-score for three-way classification
+# target_value is the value of the class to calculate F1-score for
+def ternary_f1_score(predictions, labels, target_value):
+    precision = ternary_precision(predictions, labels, target_value)
+    recall = ternary_recall(predictions, labels, target_value)
+    return calc_f1_score(precision, recall)
 
-    precision_neg = calc_precision_with_neutrals(predictions_num, labels_num, version="neg")
-    recall_neg = calc_recall_with_neutrals(predictions_num, labels_num, version="neg")
-    F1_neg = calc_F1_score(precision_neg, recall_neg)
 
-    return (F1_pos + F1_neg) / 2
+# Calculates precision for three-way classification
+def ternary_precision(predictions, labels, target_value):
+    prepared_predictions = prepare_data(predictions, target_value)
+    prepared_labels = prepare_data(labels, target_value)
+    return precision_score(y_true=prepared_labels, y_pred=prepared_predictions)
+
+
+# Calculates recall for three-way classification
+def ternary_recall(predictions, labels, target_value):
+    prepared_predictions = prepare_data(predictions, target_value)
+    prepared_labels = prepare_data(labels, target_value)
+    return recall_score(y_true=prepared_labels, y_pred=prepared_predictions)
+
+
+# Prepares lists of labels or predictions for further scoring
+# Sets the values with the target value to 1, the rest to -1
+# This way we can consider only one target value at a time
+def prepare_data(values, target_value):
+    return [1 if value == target_value else -1 for value in values]
 
 
 # Returns F1-score given precision and recall
-def calc_F1_score(precision, recall):
+def calc_f1_score(precision, recall):
     if precision + recall == 0:
         return 0
 
