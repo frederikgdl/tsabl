ac919f6d49d247a5f3f0595b5feed617775bbfde
frederikgdl <frederikgdl@gmail.com>
2017-04-12 09:03:41 -0400

    New run scripts and topic enhanced model

---
+++ SAEmbedding/run_embedding_lengths.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+WINDOW_SIZE=7
+HIDDEN_LENGTH=20
+VOCAB_FILE=FOO
+TRAIN_FILE_NUM=1
+TRAINING_ROUND=20
+LEARNING_RATE=0.1
+MARGIN=1
+RANDOM_BASE=0.01
+SENTIMENT_ALPHA=0.5
+
+INPUT_DIR='../data/preprocessed/datasets/1M/'
+
+OUTPUT_DIR='../data/embeddings/ternary/'
+
+DATA_PREFIX='tweets.'
+
+DATASET='AFINN'
+
+INPUT_PREFIX=${DATA_PREFIX}${DATASET}
+
+OUTPUT_FILE=${OUTPUT_DIR}${DATASET}'/embeddings-'${EMBEDDING_LENGTH}'-'${DATASET}
+
+for EMBEDDING_LENGTH in 50 100 150
+do
+    java -classpath bin sa_embedding.TernaryHybridRankingMain -windowSize ${WINDOW_SIZE} \
+     -hiddenLength ${HIDDEN_LENGTH} -embeddingLength ${EMBEDDING_LENGTH} \
+     -inputDir ${INPUT_DIR} -vocabFile ${VOCAB_FILE} -trainFileNum ${TRAIN_FILE_NUM} \
+     -trainingRound ${TRAINING_ROUND} -learningRate ${LEARNING_RATE} -margin ${MARGIN} \
+     -outputFile ${OUTPUT_FILE} -randomBase ${RANDOM_BASE} \
+     -sentimentAlpha ${SENTIMENT_ALPHA} -inputFilePrefix ${INPUT_PREFIX}
+done
