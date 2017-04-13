e84fa341cb21076e6538a708753cb2faef7de8c2
Mats Byrkjeland <matsbyr@gmail.com>
2017-04-13 13:08:20 +0200

    Remove unused config

--- classifiers/config.py
+++ classifiers/config.py
@@ -6,8 +6,5 @@ EMBEDDING_FILE = path.abspath("data/embeddings/embeddings.txt")
 TRAIN_FILE = path.abspath("data/preprocessed/twitter-2016train-A.txt")
 TEST_FILE = path.abspath("data/preprocessed/twitter-2016devtest-A.txt")
 
-SVM_MODEL_FILE = path.abspath("models/svm.pickle")
-LOGRES_MODEL_FILE = path.abspath("models/logres.pickle")
-
 MODELS_DIR = path.abspath("models")
 RESULTS_DIR = path.abspath("results")
