35176b26fd24e44f7ca62430f319f75620af530c
Mats Byrkjeland <matsbyr@gmail.com>
2017-04-27 09:58:28 +0200

    Plot Macro F1 and F1 PN scores

--- scripts/test_all_epochs.py
+++ scripts/test_all_epochs.py
@@ -1,22 +1,42 @@
 import classifiers.train_and_test as train_and_test
 from classifiers.models.svm import SVM
 from os import path, listdir
+import matplotlib.pyplot as plt
 
 path_of_this_file = path.dirname(path.realpath(__file__))
 
 # Directory containing embeddings of different epochs
 embeddings_dir = path.join(path_of_this_file, "../data/embeddings/binary_sa_embedding")
 
-for embeddings_file in sorted(listdir(embeddings_dir)):
+# Sort by suffix number of files. Turn to int so that '7' is treated as less that '18', for instance.
+embeddings_files = sorted(listdir(embeddings_dir), key=lambda f: int(f.split("-")[-1]))
+
+results = []
+
+for embeddings_file in embeddings_files:
+    print(embeddings_file)
+
     # Configure test_and_train
     train_and_test.classifiers = [SVM()]
     train_and_test.baselines = []
-    train_and_test.save_format = 'data'
     train_and_test.embedding_file = path.join(embeddings_dir, embeddings_file)
     train_and_test.verbose = -2
     train_and_test.quiet = True
 
-    print(embeddings_file)
-
     # Run
     train_and_test.main()
+
+    # Save results
+    result = train_and_test.classifiers[0].results.clone()
+    results.append(result)
+
+# Plot
+x = list(range(1, 1 + len(embeddings_files)))
+plt.plot(x, list(map(lambda r: r['ternary_macro_f1_score'], results)), 'b-', label='Macro F1')
+plt.plot(x, list(map(lambda r: r['f1_pn_score'], results)), 'r-', label='F1 PN')
+# plt.axis([0, len(embeddings_files), 0, 1])
+plt.xticks(x)
+plt.xlabel('Epoch')
+plt.ylabel('Scores')
+plt.legend()
+plt.show()
