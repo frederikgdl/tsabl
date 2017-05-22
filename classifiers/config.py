e4ca4893b2614e67db419a777631e104725a09f4
frederikgdl <frederikgdl@gmail.com>
2017-05-22 10:54:33 +0200

    Set final svm grid search params in config

--- classifiers/config.py
+++ classifiers/config.py
@@ -15,7 +15,7 @@ SUFFIX = "agg_ternary/LexiconClassifier/embeddings-100-LexiconClassifier-round-1
 EMBEDDING_FILE = path.abspath("data/embeddings/" + SUFFIX)
 # EMBEDDING_FILE = path.abspath("data/embeddings/embeddings.txt")
 TRAIN_FILE = path.abspath("data/preprocessed/twitter-2013train-A.txt")
-TEST_FILE = path.abspath("data/preprocessed/twitter-2013dev-A.txt")
+TEST_FILE = path.abspath("data/preprocessed/twitter-2013test-A.txt")
 
 # K-fold validation
 # Set K to a number greater than 1 to activate K-fold validation.
@@ -28,71 +28,35 @@ SKIP_TRAINING = False
 SKIP_TESTING = False
 
 MODELS_DIR = path.abspath("models")
-RESULTS_DIR = path.abspath("results/svm-tests/" + SUFFIX)
+RESULTS_DIR = path.abspath("results/" + SUFFIX)
 
 CLASSIFIERS = [
-    SVM(name="SVM c=1e-3 dual=False", c=1e-3, dual=False),
-    SVM(name="SVM c=1e-2 dual=False", c=1e-2, dual=False),
-    SVM(name="SVM c=1e-1 dual=False", c=1e-1, dual=False),
-    SVM(name="SVM c=1 dual=False", c=1, dual=False),
-    SVM(name="SVM c=1e1 dual=False", c=1e1, dual=False),
-    SVM(name="SVM c=1e2 dual=False", c=1e2, dual=False),
-    SVM(name="SVM c=1e3 dual=False", c=1e3, dual=False),
-    SVM(name="SVM c=2e-1 dual=False", c=2e-1, dual=False),
-    SVM(name="SVM c=3e-1 dual=False", c=3e-1, dual=False),
-    SVM(name="SVM c=4e-1 dual=False", c=4e-1, dual=False),
-    SVM(name="SVM c=5e-1 dual=False", c=5e-1, dual=False),
-    SVM(name="SVM c=6e-1 dual=False", c=6e-1, dual=False),
-    SVM(name="SVM c=7e-1 dual=False", c=7e-1, dual=False),
-    SVM(name="SVM c=8e-1 dual=False", c=8e-1, dual=False),
-    SVM(name="SVM c=9e-1 dual=False", c=9e-1, dual=False),
-    SVM(name="SVM c=2e-2 dual=False", c=2e-2, dual=False),
-    SVM(name="SVM c=3e-2 dual=False", c=3e-2, dual=False),
-    SVM(name="SVM c=4e-2 dual=False", c=4e-2, dual=False),
-    SVM(name="SVM c=5e-2 dual=False", c=5e-2, dual=False),
-    SVM(name="SVM c=6e-2 dual=False", c=6e-2, dual=False),
-    SVM(name="SVM c=7e-2 dual=False", c=7e-2, dual=False),
-    SVM(name="SVM c=8e-2 dual=False", c=8e-2, dual=False),
-    SVM(name="SVM c=9e-2 dual=False", c=9e-2, dual=False),
-    SVM(name="SVM c=1.1 dual=False", c=1.1, dual=False),
-    SVM(name="SVM c=1.2 dual=False", c=1.2, dual=False),
-    SVM(name="SVM c=1.3 dual=False", c=1.3, dual=False),
-    SVM(name="SVM c=1.4 dual=False", c=1.4, dual=False),
-    SVM(name="SVM c=1.5 dual=False", c=1.5, dual=False),
-    SVM(name="SVM c=1.6 dual=False", c=1.6, dual=False),
-    SVM(name="SVM c=1.7 dual=False", c=1.7, dual=False),
-    SVM(name="SVM c=1.8 dual=False", c=1.8, dual=False),
-    SVM(name="SVM c=1.9 dual=False", c=1.9, dual=False),
-    SVM(name="SVM c=2.0 dual=False", c=2.0, dual=False),
-    # SVM(name="SVM c=1 dual=False", c=2e-2, dual=False),
-    # SVM(name="SVM c=1 dual=True", c=2e-2, dual=True),
-    # SVM(name="SVM c=2e-2", c=2e-2),
-    # SVM(name="SVM c=1", c=1),
-    # SVM(name="SVM c=1e-3", c=1e-3),
-    # SVM(name="SVM c=1e-2", c=1e-2),
-    # SVM(name="SVM c=2e-2", c=2e-2),
-    # SVM(name="SVM c=3e-2", c=3e-2),
-    # SVM(name="SVM c=4e-2", c=4e-2),
-    # SVM(name="SVM c=5e-2", c=5e-2),
-    # SVM(name="SVM c=6e-2", c=6e-2),
-    # SVM(name="SVM c=7e-2", c=7e-2),
-    # SVM(name="SVM c=8e-2", c=8e-2),
-    # SVM(name="SVM c=9e-2", c=9e-2),
-    # SVM(name="SVM c=2e-1", c=2e-1),
-    # SVM(name="SVM c=3e-1", c=3e-1),
-    # SVM(name="SVM c=4e-1", c=4e-1),
-    # SVM(name="SVM c=5e-1", c=5e-1),
-    # SVM(name="SVM c=6e-1", c=6e-1),
-    # SVM(name="SVM c=7e-1", c=7e-1),
-    # SVM(name="SVM c=8e-1", c=8e-1),
-    # SVM(name="SVM c=9e-1", c=9e-1),
-    # SVM(name="SVM c=1e-1", c=1e-1),
-    # SVM(name="SVM c=1e1", c=1e1),
-    # SVM(name="SVM c=1e2", c=1e2),
-    # SVM(name="SVM c=1e3", c=1e3),
+    SVM(name="SVM c=6e-3 dual=False", c=6e-3, dual=False),
+    # SVM(name="SVM c=1e-3 dual=False", c=1e-3, dual=False),
+    # SVM(name="SVM c=1e-2 dual=False", c=1e-2, dual=False),
+    # SVM(name="SVM c=1e-1 dual=False", c=1e-1, dual=False),
+    # SVM(name="SVM c=1 dual=False", c=1, dual=False),
+    # SVM(name="SVM c=1e1 dual=False", c=1e1, dual=False),
+    # SVM(name="SVM c=1e2 dual=False", c=1e2, dual=False),
+    # SVM(name="SVM c=1e3 dual=False", c=1e3, dual=False),
+    # SVM(name="SVM c=2e-3 dual=False", c=2e-3, dual=False),
+    # SVM(name="SVM c=3e-3 dual=False", c=3e-3, dual=False),
+    # SVM(name="SVM c=4e-3 dual=False", c=4e-3, dual=False),
+    # SVM(name="SVM c=5e-3 dual=False", c=5e-3, dual=False),
+    # SVM(name="SVM c=6e-3 dual=False", c=6e-3, dual=False),
+    # SVM(name="SVM c=7e-3 dual=False", c=7e-3, dual=False),
+    # SVM(name="SVM c=8e-3 dual=False", c=8e-3, dual=False),
+    # SVM(name="SVM c=9e-3 dual=False", c=9e-3, dual=False),
+    # SVM(name="SVM c=2e-2 dual=False", c=2e-2, dual=False),
+    # SVM(name="SVM c=3e-2 dual=False", c=3e-2, dual=False),
+    # SVM(name="SVM c=4e-2 dual=False", c=4e-2, dual=False),
+    # SVM(name="SVM c=5e-2 dual=False", c=5e-2, dual=False),
+    # SVM(name="SVM c=6e-2 dual=False", c=6e-2, dual=False),
+    # SVM(name="SVM c=7e-2 dual=False", c=7e-2, dual=False),
+    # SVM(name="SVM c=8e-2 dual=False", c=8e-2, dual=False),
+    # SVM(name="SVM c=9e-2 dual=False", c=9e-2, dual=False),
     # LogRes()
 ]
-
 
 BASELINES = [
     RandomUniform(),
